b2ac4872ff815772cbb9527dec9ba24f18485e23
Jiliana Tan <136918826+jilianatan@users.noreply.github.com>
2024-09-08 21:47:16 +0800

    Finalize

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -13,9 +13,19 @@ void exit();
 int main() {
     std::string command;
 
-    while (command != "Exit") {
+    while (command != "exit") {
 
-        std::cout << "\n[ DISPLAY HEADER ]\n";
+        std::cout << " _______  _______  _______  _______  _______  _______  __   __  \n";
+        std::cout << "|       ||       ||       ||       ||       ||       ||  | |  |\n";
+        std::cout << "|       ||  _____||   _   ||    _  ||    ___||  _____||  |_|  | \n";
+        std::cout << "|       || |_____ |  | |  ||   |_| ||   |___ | |_____ |       |\n";
+        std::cout << "|      _||_____  ||  |_|  ||    ___||    ___||_____  ||_     _|\n";
+        std::cout << "|     |_  _____| ||       ||   |    |   |___  _____| |  |   |   \n";
+        std::cout << "|_______||_______||_______||___|    |_______||_______|  |___|  \n\n";
+                                           
+
+
+        
         std::cout << "List of Commands:\n * Initialize\n * Screen\n * Schedule-Tester\n * Schedule-Stop\n * Report-Util\n * Clear\n * Exit\n";
         std::cout << "\nEnter a command: ";
         std::getline(std::cin, command);
@@ -37,6 +47,16 @@ int main() {
         else if (command == "report-util") {
             reportUtil();
         }
+        else if (command == "clear") {
+            system("cls");
+        } 
+        else if (command == "exit") {
+        }
+        else {
+            std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+            std::cout << "Command not recognized. \n";
+            std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+        }
 
     }
 
@@ -44,21 +64,31 @@ int main() {
 
 void initialize() {
 
-    std::cout << "Initialize command recognized. Doing something.";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+    std::cout << "Initialize command recognized. Doing something.\n";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
 }
 
 void screen() {
-    std::cout << "Screen command recognized. Doing something.";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+    std::cout << "Screen command recognized. Doing something.\n";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
 }
 
 void scheduleTester() {
-    std::cout << "Schedule Tester command recognized. Doing something.";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+    std::cout << "Schedule Tester command recognized. Doing something.\n";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
 }
 
 void schedulerStop() {
-    std::cout << "Scheduler Stop command recognized. Doing something.";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+    std::cout << "Scheduler Stop command recognized. Doing something.\n";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
 }
 
 void reportUtil() {
-    std::cout << "Report Util command recognized. Doing something.";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
+    std::cout << "Report Util command recognized. Doing something.\n";
+    std::cout << "\n^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^\n\n";
 }
