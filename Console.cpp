bbb753be393e26c239ea7c2dd118295157231ccd
Jiliana Tan <136918826+jilianatan@users.noreply.github.com>
2024-09-25 21:47:05 +0800

    edit

--- Console.cpp
+++ Console.cpp
@@ -19,11 +19,13 @@ std::string Console::getName() const {
 }
 void Console::drawMain() {
 
-	std::cout << "  ____ ____   ___  ____  _____ ______   __\n"
-		" / ___/ ___| / _ \\|  _ \\| ____/ ___\\ \\ / /\n"
-		"| |   \\___ \\| | | | |_) |  _| \\___ \\\\ V / \n"
-		"| |___ ___) | |_| |  __/| |___ ___) || |  \n"
-		" \\____|____/ \\___/|_|   |_____|____/ |_|  \n";
+	std::cout << " _______  _______  _______  _______  _______  _______  __   __  \n";
+	std::cout << "|       ||       ||       ||       ||       ||       ||  | |  |\n";
+	std::cout << "|       ||  _____||   _   ||    _  ||    ___||  _____||  |_|  | \n";
+	std::cout << "|       || |_____ |  | |  ||   |_| ||   |___ | |_____ |       |\n";
+	std::cout << "|      _||_____  ||  |_|  ||    ___||    ___||_____  ||_     _|\n";
+	std::cout << "|     |_  _____| ||       ||   |    |   |___  _____| |  |   |   \n";
+	std::cout << "|_______||_______||_______||___|    |_______||_______|  |___|  \n\n";
 
 	std::cout << "\033[1;32m"; // 1;32m represents bold green text
 	std::cout << "Hi, Welcome to the CSOPESY command line!" << std::endl;
@@ -31,7 +33,6 @@ void Console::drawMain() {
 	std::cout << "\033[1;33m"; // 1;33m represents bold yellow text
 	std::cout << "Type 'exit' to quit, 'clear' to clear the screen" << std::endl;
 	std::cout << "\033[0m"; // Reset text color to default
-
 
 }
 void Console::drawProcess() {
